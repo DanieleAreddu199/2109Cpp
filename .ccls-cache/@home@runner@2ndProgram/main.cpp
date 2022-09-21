3b90f43f340c53eb8983391f21e6859a6158a8c8
DANIELE MARIO MARIO <113968527+DanieleAreddu199@users.noreply.github.com>
2022-09-21 08:03:55 +0000

    Aggiunta funzione per la media diviso 4

--- .ccls-cache/@home@runner@2ndProgram/main.cpp
+++ .ccls-cache/@home@runner@2ndProgram/main.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 
 int main() {
-    float x, y, z, media;
+    float x, y, z, v, media;
     cout<<"Inserisci il primo numero: \t";
     cin>>x;
     cout<<"Inserisci il secondo numero: \t";
     cin>>y;
     cout<<"Inserisci il terzo numero: \t";
     cin>>z;
-    media=(x+y+z)/3;
-    cout<<"La media dei tre numeri e': \t"<<media<<endl;
+    cout<<"Inserisci il quarto numero \t" ;
+    cin>>v;
+    media=(x+y+z+v)/4;
+    cout<<"La media dei quattro numeri e': \t"<<media<<endl;
     return 0;
 }
