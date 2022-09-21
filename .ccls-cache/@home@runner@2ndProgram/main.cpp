8fc7b31bfda975dfc022322c837f87fe5d77be07
DANIELE MARIO MARIO <113968527+DanieleAreddu199@users.noreply.github.com>
2022-09-21 07:44:47 +0000

    calcolatrice per la media

--- .ccls-cache/@home@runner@2ndProgram/main.cpp
+++ .ccls-cache/@home@runner@2ndProgram/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello 3d 21/09/22 !\n";
+    float x, y, z, media;
+    cout<<"Inserisci il primo numero: \t";
+    cin>>x;
+    cout<<"Inserisci il secondo numero: \t";
+    cin>>y;
+    cout<<"Inserisci il terzo numero: \t";
+    cin>>z;
+    media=(x+y+z)/3;
+    cout<<"La media dei tre numeri e': \t"<<media<<endl;
+    return 0;
 }
